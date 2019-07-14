daa81ba08a6b347d9d0b48c635de5c11e59a444f
Omar <omar.4hmedk@gmail.com>
2019-07-14 15:03:11 +0200

    created index,base,signup,login files (unfinished)

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, jsonify, render_template, url_for, redirect, flash
+from flask_wtf.csrf import CSRFProtect
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
